40c7b19f4e6261423e6e257421cc1579fa057811
Ben O'Neill <ben@benoneill.xyz>
2021-09-23 09:28:53 -0400

    simplify worduse somewhat

--- worduse.sh
+++ worduse.sh
@@ -2,4 +2,4 @@
 # worduse: calculate word frequency from standard input
 # usage: worduse
 
-sed 's/--/ /' | tr -cd '[:alpha:] \n' | tr '[:upper:]' '[:lower:]' | awk -v OFS='\n' '{$1=$1}1' | tr -d ' \t' | sort | uniq -c | sort -n
+tr -cd "[:alpha:] \n" | tr '[:upper:] ' '[:lower:]\n' | sort | uniq -c | sort -n | sed '$d'
